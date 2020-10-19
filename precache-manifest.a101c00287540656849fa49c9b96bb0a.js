334c4155f2019d343354de69c05fdc28e7732799
Tanya Margulis <Tany999m@gmail.com>
2020-10-19 23:14:20 +0200

    Updates

---
+++ precache-manifest.a101c00287540656849fa49c9b96bb0a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c2670009fe95a2dc4d0b5eba09b3ed",
+    "url": "/react-around-auth/index.html"
+  },
+  {
+    "revision": "9a5ff06d062101c9f842",
+    "url": "/react-around-auth/static/css/main.a15ecd8c.chunk.css"
+  },
+  {
+    "revision": "cce075a0785a917146a8",
+    "url": "/react-around-auth/static/js/2.810fa8e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-around-auth/static/js/2.810fa8e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5ff06d062101c9f842",
+    "url": "/react-around-auth/static/js/main.e4dd8b46.chunk.js"
+  },
+  {
+    "revision": "5f3478028a5dc90c89ef",
+    "url": "/react-around-auth/static/js/runtime-main.c8c10083.js"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/react-around-auth/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "b82eef497995d28fffc2ef185211e282",
+    "url": "/react-around-auth/static/media/Inter-ExtraBold.b82eef49.woff"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/react-around-auth/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/react-around-auth/static/media/add.bdfb8ebb.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/react-around-auth/static/media/black-heart.9fdebb6b.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/react-around-auth/static/media/close-icon.c2d0901f.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/react-around-auth/static/media/edit.58afbe73.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/react-around-auth/static/media/heart.6c74d9b5.svg"
+  },
+  {
+    "revision": "3e6893cd2eab107d2d5385c921e95382",
+    "url": "/react-around-auth/static/media/logo.3e6893cd.svg"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "/react-around-auth/static/media/trash.9c3b03ae.svg"
+  }
+]);
